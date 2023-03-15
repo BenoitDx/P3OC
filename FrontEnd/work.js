87bbd3ffa5798bc5464e7080fd2a127986cc79b2
benoit dx <benoit.berche@gmail.com>
2023-03-15 13:17:48 +0100

    ajout dynamique img

--- FrontEnd/work.js
+++ FrontEnd/work.js
@@ -1,18 +1,19 @@
-const gallery = document.querySelector('.gallery');
 
-fetch('http://localhost:5678/api/works')
+fetch("http://localhost:5678/api/works")
   .then(response => response.json())
   .then(data => {
-    data.forEach(image => {
-      const figure = document.createElement('figgure');
-      const img = document.createElement('img');
-      img.src = image.url;
-      img.alt = image.title;
-      const figcaption = document.createElement('figcaption');
-      figcaption.textContent = image.title;
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
+   
+    const imageUrls = data.map(work => work.imageUrl);
+
+    const imageElements = imageUrls.map(url => {
+      const img = document.createElement("img");
+      img.src = url;
+      return img;
+    });
+
+    const imageContainer = document.querySelector(".gallery");
+    imageElements.forEach(img => {
+      imageContainer.appendChild(img);
     });
   })
   .catch(error => console.error(error));
