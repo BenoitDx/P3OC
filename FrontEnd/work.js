5340d2c4d132e14f20bc807eadb919122225128e
benoit dx <benoit.berche@gmail.com>
2023-03-15 18:10:29 +0100

    fonctionnement img dyn

--- FrontEnd/work.js
+++ FrontEnd/work.js
@@ -1,19 +1,23 @@
-
 fetch("http://localhost:5678/api/works")
   .then(response => response.json())
   .then(data => {
-   
-    const imageUrls = data.map(work => work.imageUrl);
-
-    const imageElements = imageUrls.map(url => {
+    const imageElements = data.map(work => {
+      const imgContainer = document.createElement("div");
+      imgContainer.classList.add("image-container");
       const img = document.createElement("img");
-      img.src = url;
-      return img;
+      img.src = work.imageUrl;
+      imgContainer.appendChild(img);
+      const title = document.createElement("div");
+      title.classList.add("title");
+      title.textContent = work.title;
+      imgContainer.appendChild(title);
+      return imgContainer;
     });
 
     const imageContainer = document.querySelector(".gallery");
     imageElements.forEach(img => {
       imageContainer.appendChild(img);
     });
+
+    console.log("Données récupérées !");
   })
-  .catch(error => console.error(error));
