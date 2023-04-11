5108a8316db1e1fdf96e125a7aa66a7681d55f5b
benoit dx <benoit.berche@gmail.com>
2023-04-11 18:35:50 +0200

    finalisation

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -68,6 +68,8 @@ fetch("http://localhost:5678/api/works")
       imgContainer.classList.add("modal-image-container");
       const img = document.createElement("img");
       img.src = work.imageUrl;
+       // Ajout de l'attribut data-id contenant l'ID de l'image
+      img.setAttribute("data-id", work.id);
       imgContainer.appendChild(img);
       // création d'un bouton Editer
       const editor = document.createElement('span');
@@ -84,9 +86,124 @@ fetch("http://localhost:5678/api/works")
       enlarge.setAttribute('class', 'enlarge');
       enlarge.src = "assets/icons/Move.Png";
       imgContainer.appendChild(enlarge);
+// Ajout d'un gestionnaire d'événement à l'icône de la corbeille pour supprimer une image
+const bins = document.querySelectorAll(".bin");
+bins.forEach(bin => {
+  bin.addEventListener("click", function() {
+    const id = this.parentNode.querySelector("img").getAttribute("data-id"); // récupère l'ID de l'image à partir de l'attribut data-id de l'image
+    fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${token}`, // ajoute le token d'authentification à l'en-tête de la requête
+        "Content-Type": "application/json" // spécifie le type de contenu de la requête
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        // Suppression réussie - retire l'image de la galerie
+        const imageContainer = this.parentNode;
+        imageContainer.remove();
+         // Supprime également l'image de la page principale
+         const mainImage = document.querySelector(`[data-id='${id}']`).parentNode;
+         mainImage.remove();
+      } else {
+        // Erreur lors de la suppression
+        console.log(`Erreur de suppression : ${response.status} - ${response.statusText}`);
+        return response.json(); // convertit la réponse en objet JSON
+      }
+    })
+    .then(data => {
+      // traite les données renvoyées par l'API
+      console.log(data);
+    })
+    .catch(error => console.log(`Erreur de suppression : ${error.message}`));
+  });
+});
+
+
+
       // ajout de l'image à la div gallery-modal
+
       const galleryModal = document.querySelector(".gallery-modal");
       galleryModal.appendChild(imgContainer);
     });
+    
   });
+
+// Récupération du bouton "Ajouter une photo" de la modal galerie photo
+const addImageBtn = document.querySelector('#add-image');
+
+// Récupération de la modal galerie photo et de la modal post
+const modalGalerie = document.querySelector('#modal');
+const modalPost = document.querySelector('#modal-post');
+
+// Ajout d'un gestionnaire d'événement au clic sur le bouton "Ajouter une photo"
+addImageBtn.addEventListener('click', () => {
+  // Fermeture de la modal galerie photo
+  modalGalerie.style.display = 'none';
+  // Ouverture de la modal post
+  modalPost.style.display = 'flex';
+  
+  // Récupérer l'icône "back"
+const backButton = document.querySelector('#modal-post .back');
+
+// Ajouter un écouteur d'événement pour le clic sur l'icône "back"
+backButton.addEventListener('click', function() {
+  // Fermer la modal post
+  const modalPost = document.querySelector('#modal-post');
+  modalPost.style.display = 'none';
+
+  // Ouvrir la modal gallery
+  const modalGallery = document.querySelector('#modal');
+  modalGallery.style.display = 'flex';
+});
+
+// Récupérer l'icône "close" pour fermer la modal post
+const closeButtonPost = document.querySelector('#modal-post .closepost');
+
+// Ajouter un écouteur d'événement pour le clic sur l'icône "close"
+closeButtonPost.addEventListener('click', function() {
+  // Fermer la modal post
+  const modalPost = document.querySelector('#modal-post');
+  modalPost.style.display = 'none';
+});
+
+// Récupérer l'icône "close" pour fermer la modal gallery
+const closeButtonGallery = document.querySelector('#modal .close');
+
+// Ajouter un écouteur d'événement pour le clic sur l'icône "close"
+closeButtonGallery.addEventListener('click', function() {
+  // Fermer la modal gallery
+  const modalGallery = document.querySelector('#modal');
+  modalGallery.style.display = 'none';
+});
+
+  // Ajout d'un gestionnaire d'événement à l'élément window pour fermer la modal lorsqu'on clique en dehors de la fenêtre
+  window.addEventListener("click", function(event) {
+    // Sélection de l'élément modal
+    const modal = document.getElementById("modal-post");
+    if (event.target === modal) {
+      // Modification de la propriété "display" pour masquer la modal
+      modal.style.display = "none";
+    }
+  });
+  
+});
+
+const fileInput = document.getElementById('file-input');
+const imagePreview = document.getElementById('image-preview');
+
+fileInput.onchange = () => {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    imagePreview.style.display = 'block';
+    imagePreview.setAttribute('src', reader.result);
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
 
